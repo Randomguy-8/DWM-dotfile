edf55bf51fbc56d881833fa403de5dd05077c2a6
Random Guy <randomguyx9699@gmail.com>
2022-07-04 10:17:09 +0530

    Add files via upload

--- COPY.sh
+++ COPY.sh
@@ -2,4 +2,8 @@
 
 
 cp -r HOME/* ~/
-cp -r bin/* /bin
+cp -r bin/* /bin/
+unzip usr/share/themes/Arc-Dark.zip && mv Arc-Dark/ usr/share/themes/
+cp -r usr/share/themes/Arc-Dark /usr/share/themes/
+
+
